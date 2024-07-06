8beffa3870e8aaccfa0dac50c6fd5b6020ba8eba
lauramoura98 <lauramoura06@gmail.com>
2024-07-06 13:42:49 -0300

    creating info json

--- api_requests.py
+++ api_requests.py
@@ -1,9 +1,12 @@
 import requests 
 import os
 from dotenv import load_dotenv
-
+import json
 load_dotenv()
+
 polygon_api_key = os.getenv('POLYGON_API')
+opensea_api_key = os.getenv('OPEN_SEA_API')
+contract_address = os.getenv('COSMO_CONTRACT_ADDRESS')
 wallet_address = '0x15572b0f6e6DD6ACFC083de45A390827F431d20d'
 
 def get_transfers(address: str, key: str):
@@ -24,9 +27,33 @@ def get_token_id():
         if transfer['to'].lower() == wallet_address.lower() and transfer['tokenID'] not in seen_token_ids:
             transfers.append(transfer['tokenID'])
             seen_token_ids.add(transfer['tokenID'])
-            count += 1
-            if count == 10:
-                break
     return transfers
 
-print(get_token_id())
+
+def get_objekt_info(contract: str, key: str, token_id: str):
+    url = f'https://api.opensea.io/api/v2/chain/matic/contract/{contract}/nfts/{token_id}'
+    headers = {
+        "accept": "application/json",
+        "x-api-key": key
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def create_objekts_dict():
+    token_ids = get_token_id()
+    objekts = {}
+    for i in range(10):
+        objekt_info = get_objekt_info(contract_address, opensea_api_key, token_ids[i])
+        nft_data = objekt_info['nft']
+        objekts[nft_data['identifier']] = {
+            'name': nft_data['name'],
+            'image_url': nft_data['image_url']
+        }
+    return objekts
+
+objekts = create_objekts_dict()
+
+with open('objekts.json', 'w') as json_file:
+    json.dump(objekts, json_file, indent=4)
+
+print("Arquivo JSON criado com sucesso.")
