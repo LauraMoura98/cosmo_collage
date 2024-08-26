4de51a8f7aee557b3b111f8fb5edd6aee9193650
lauramoura98 <lauramoura06@gmail.com>
2024-08-26 19:35:10 -0300

    fixing transfers logic

--- api_requests.py
+++ api_requests.py
@@ -1,7 +1,7 @@
 import requests 
 import os
 from dotenv import load_dotenv
-import json
+
 load_dotenv()
 
 polygon_api_key = os.getenv('POLYGON_API')
@@ -9,26 +9,34 @@ opensea_api_key = os.getenv('OPEN_SEA_API')
 contract_address = os.getenv('COSMO_CONTRACT_ADDRESS')
 wallet_address = '0x15572b0f6e6DD6ACFC083de45A390827F431d20d'
 
-def get_transfers(address: str, key: str):
+def get_wallet_transfers(address: str, key: str):
+    """ Return wallet transfer infos by making a HTTP request on PolygonScan API """
+
     url = f'https://api.polygonscan.com/api?module=account&action=tokennfttx&' \
           f'address={address}&startblock=0&endblock=99999999&page=1&offset=1000&' \
           f'sort=desc&apikey={key}'
 
     transfer_result = requests.get(url).json()
+
     return transfer_result['result']
 
+def get_tranfers_token_id():
+    """ Extracts Token IDs from wallet transfers and makes verification whether a objekt still in main user's wallet or not"""
 
-def get_token_id():
-    get_polygon_transfers = get_transfers(wallet_address, polygon_api_key)
-    transfers = []
-    seen_token_ids = set()
-    count = 0
-    for transfer in get_polygon_transfers:
-        if transfer['to'].lower() == wallet_address.lower() and transfer['tokenID'] not in seen_token_ids:
-            transfers.append(transfer['tokenID'])
-            seen_token_ids.add(transfer['tokenID'])
-    return transfers
+    wallet_transfers_info = get_wallet_transfers(wallet_address, polygon_api_key)
 
+    cosmo_transfers = []
+    users_transfers =[]
+
+    for transfer in wallet_transfers_info:
+            if transfer['from'].lower() == contract_address.lower():
+                cosmo_transfers.append(transfer['tokenID'])
+            elif transfer['from'].lower() != wallet_address.lower():
+                users_transfers.append(transfer['tokenID'])
+
+    return users_transfers, cosmo_transfers
+
+print(get_tranfers_token_id())
 
 def get_objekt_info(contract: str, key: str, token_id: str):
     url = f'https://api.opensea.io/api/v2/chain/matic/contract/{contract}/nfts/{token_id}'
@@ -53,7 +61,7 @@ def create_objekts_dict():
 
 objekts = create_objekts_dict()
 
-with open('objekts.json', 'w') as json_file:
-    json.dump(objekts, json_file, indent=4)
+# with open('objekts.json', 'w') as json_file:
+#     json.dump(objekts, json_file, indent=4)
 
-print("Arquivo JSON criado com sucesso.")
+# print("Arquivo JSON criado com sucesso.")
